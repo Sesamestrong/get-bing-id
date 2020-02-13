d8edf5f9d1d7080259f68c2fc49e16357f230986
Andrew Healey <doolie.healey@gmail.com>
2020-02-13 18:44:49 -0500

    I am stupid

--- index.js
+++ index.js
@@ -12,7 +12,7 @@ app.use(bodyParser.urlencoded({
 app.post("/",async (req,res)=>{
     const {day,month,year} = req.body;
     const daysSinceThen=moment.duration(moment().startOf("day").diff(moment(`${year}-${month}-${day}`,"YYYY-MM-DD"))).days();
-    res.text(daysSinceThen);
+    res.send(`${daysSinceThen}`);
 });
 
 const PORT=process.env.PORT || 3000;
